2c050432b64e6166d669c34c348720c3ffd76192
earbis <jabyruiz@gmail.com>
2023-09-26 10:10:48 +0200

    Trabajo de Clase

---
+++ Ejercicios/tema1/EscritorConBuffer.java
@@ -0,0 +1,22 @@
+package tema1;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class EscritorConBuffer {
+	public static void main(String[] args) throws IOException {
+		File fichero = new File(".\\LeerFicheroTexto.txt");
+			File copia = new File(".\\CopiaFicheroTexto.txt");
+			FileReader fic = new FileReader(fichero);
+			FileWriter cop =new FileWriter(copia);
+			char b[] = new char[2];
+			int i;
+			while ((i = fic.read(b)) != -1) {
+				System.out.println(b);
+				cop.append((char) b[i-1]);}
+			fic.close();
+			cop.close();
+		}
+	}
