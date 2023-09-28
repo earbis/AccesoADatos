afd6cf714f39724aeffefb5daecc005b4db3dbf0
earbis <jabyruiz@gmail.com>
2023-09-28 13:30:10 +0200

    Create LeerFichTexto.java

---
+++ Ejercicios/tema1/LeerFichTexto.java
@@ -0,0 +1,13 @@
+package tema1;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+public class LeerFichTexto {
+	public static void main(String[] args) throws FileNotFoundException {
+		File fichero = new File("C:\\Users\\Earbis\\eclipse-workspace\\programacionyServicios\\ejercicios\\tema1");
+		FileReader fic = new FileReader(fichero);
+		
+	}
+}
