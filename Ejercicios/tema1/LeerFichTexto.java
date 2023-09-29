748f383f21cc4e2a07db8dec1149ddf6b057025e
earbis <jabyruiz@gmail.com>
2023-09-29 10:28:02 +0200

    Update LeerFichTexto.java

--- Ejercicios/tema1/LeerFichTexto.java
+++ Ejercicios/tema1/LeerFichTexto.java
@@ -1,13 +1,17 @@
 package tema1;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 
 public class LeerFichTexto {
-	public static void main(String[] args) throws FileNotFoundException {
+	public static void main(String[] args) throws IOException {
 		File fichero = new File("C:\\Users\\Earbis\\eclipse-workspace\\programacionyServicios\\ejercicios\\tema1");
 		FileReader fic = new FileReader(fichero);
-		
+		int i;
+		while ((i=fic.read())!=-1) {
+			System.out.println((char) i);
+			fic.close();
+		}
 	}
 }
