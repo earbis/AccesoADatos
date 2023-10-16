701183498cc271a2fbff4df554780068ffa94fba
earbis <jabyruiz@gmail.com>
2023-10-16 12:59:43 +0200

    Update Persona.java

--- Ejercicios/tema1/Persona.java
+++ Ejercicios/tema1/Persona.java
@@ -1,6 +1,8 @@
 package tema1;
 
-public class Persona {
+import java.io.Serializable;
+
+public class Persona implements Serializable{
 	private String nombre;
 	private int edad;
 	public Persona(String nombre, int edad) {
