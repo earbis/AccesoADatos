53adbdbdf76b121eceec9193eb326d3d98441c20
earbis <jabyruiz@gmail.com>
2023-10-25 08:51:49 +0200

    consulta

---
+++ Ejercicios/tema2/Consulta1.java
@@ -0,0 +1,23 @@
+package tema2;
+
+import com.db4o.Db4oEmbedded;
+import com.db4o.ObjectContainer;
+import com.db4o.ObjectSet;
+
+public class Consulta1 {
+static String BDPer = "DBPersonas.yap";
+public static void main(String[] args) {
+	ObjectContainer db = Db4oEmbedded.openFile(Db4oEmbedded.newConfiguration(), BDPer);
+	Persona per = new Persona(null, null);
+	ObjectSet<Persona> result = db.queryByExample(per);
+	if (result.size()== 0) {
+		System.out.println("No existen registros de Personas...");
+	} else {
+		System.out.printf("Numero de registros: %d %n",result.size());
+		while(result.hasNext()) {
+			Persona p = result.next();
+			System.out.printf("Nombre: %s, Ciudad: %s %n", p.getNombre(), p.getLoc());
+		}
+	}
+}
+}
